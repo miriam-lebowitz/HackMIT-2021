76d22bc1b72b327ae440d7bab775e54c02ddc0a6
boraks4 <borak@wisc.edu>
2021-09-18 13:10:05 -0500

    started code + made json template

--- src/project.js
+++ src/project.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './project.css';
+
+class Button extends React.Component {
+
+}
+
+class Page extends React.Component {
+
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
