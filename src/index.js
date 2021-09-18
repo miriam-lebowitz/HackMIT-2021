0d0eeec18ebef4c9e4f295ebb79415d60ca90d58
miria <miriam@finaltouch.com>
2021-09-18 14:12:21 -0500

    Firebase Updates?

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import firebase from 'firebase/compat/app';
+// import firebase from "firebase/app";
 
+// Use your config values here.
+firebase.initializeApp({
+  apiKey: "AIzaSyB3u8eZU_XqJ9c3nZL3oU3rEReEVltulYI",
+  authDomain: "greenerme2021.firebaseapp.com",
+  projectId: "greenerme2021",
+  storageBucket: "greenerme2021.appspot.com",
+  messagingSenderId: "910658120246",
+  appId: "1:910658120246:web:4af8f754ce389c4ad50697",
+  measurementId: "G-0PHQ9RBQ6F"
+});
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
