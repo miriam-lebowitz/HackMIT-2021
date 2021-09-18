c433abca57357944597da099cd50026b0a3d174f
boraks4 <borak@wisc.edu>
2021-09-18 15:09:54 -0500

    prettier buttons

--- src/project.js
+++ src/project.js
@@ -6,11 +6,11 @@ class ButtonRow extends React.Component {
     render() {
         return (
             <div class={"button-row"}>
-                <input type="button" class="button-freq" id={"button-freq1"}>Never</input>
-                <input type="button" class="button-freq" id={"button-freq2"}>Rarely</input>
-                <input type="button" class="button-freq" id={"button-freq3"}>Sometimes</input>
-                <input type="button" class="button-freq" id={"button-freq4"}>Often</input>
-                <input type="button" class="button-freq" id={"button-freq5"}>Always</input>
+                <input type="button" className="button-freq" id={"button-freq1"}>Never</input>
+                <input type="button" className="button-freq" id={"button-freq2"}>Rarely</input>
+                <input type="button" className="button-freq" id={"button-freq3"}>Sometimes</input>
+                <input type="button" className="button-freq" id={"button-freq4"}>Often</input>
+                <input type="button" className="button-freq" id={"button-freq5"}>Always</input>
             </div>
         )
     }
